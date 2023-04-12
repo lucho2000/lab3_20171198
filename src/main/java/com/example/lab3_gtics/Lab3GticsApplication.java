c63723fda03b2d5872111764e24c55bc62376967
lucho2000 <a20201696@pucp.edu.pe>
2023-04-11 20:40:46 -0500

    primer commit

---
+++ src/main/java/com/example/lab3_gtics/Lab3GticsApplication.java
@@ -0,0 +1,13 @@
+package com.example.lab3_gtics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab3GticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab3GticsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/lab3_gtics/Lab3GticsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab3_gtics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab3GticsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
